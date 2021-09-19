a633d1fdf0fc29144f82ce1b6ac06e90df669b93
CodigoDeSenior <migueljnm840@gmail.com>
2021-09-19 14:48:14 -0300

    Remove class from Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,28 +9,19 @@ export function Navbar() {
 
     return (
         <BrowserRouter>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container-fluid">
-                    <button
-                        class="navbar-toggler"
-                        type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#navbarNav"
-                        aria-controls="navbarNav"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation"
-                    >
-                        <span class="navbar-toggler-icon"></span>
+            <nav>
+                <div>
+                    <button>
+                        <span></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav">
+                    <div>
+                        <ul>
                             {routesMenu
                                 .filter((route) => route.isVisible)
                                 .map((route) => {
                                     return (
-                                        <li class="nav-item">
+                                        <li>
                                             <Link
-                                                class="nav-link active"
                                                 aria-current="page"
                                                 to={route.path}
                                             >
